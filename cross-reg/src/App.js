1db88ee6f65ce066d4d0b706e92d97c638882fee
Ivette <car16020@uvg.edu.gt>
2019-08-12 11:16:34 -0600

    setup

---
+++ cross-reg/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+//https://github.com/creativetimofficial/light-bootstrap-dashboard-react
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="primary">Primary</Button>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
