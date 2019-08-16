bf98106a107c1ec1ff8ab8685feb11bd40492046
Ivette <car16020@uvg.edu.gt>
2019-08-16 07:54:22 -0600

    xslx

--- cross-reg/src/App.js
+++ cross-reg/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Selection from './pages/Selection/index';
 import Button from 'react-bootstrap/Button';
 //https://github.com/creativetimofficial/light-bootstrap-dashboard-react
 
@@ -26,6 +26,19 @@ function App() {
           <li><a href="#Checklist">Checklist</a></li>
           <li><a href="#State">Estado</a></li>
         </ol>
+        <div class="row">
+          <div class="col-md-12">
+          <p>Selecciona el nombre del estándar, regulación o marco legal </p>
+            <select class="mdb-select colorful-select dropdown-primary md-form" multiple searcable="Buscar">
+              <option value="1">Acuerdo No.006-2011</option>
+              <option value="1">NIST - Cybersecurity Framework</option>
+              <option value="1">GDC (República Dominicana)</option>
+              <option value="1">PCI</option>
+              <option value="1">ISO-27000</option>
+            </select>
+          </div>
+        </div>
+        <Selection />
       </div>
     </div>
   );
---
+++ cross-reg/src/routes/index.js
@@ -0,0 +1,5 @@
+export const HOME = '/';
+export const SELECTION = '/selection';
+export const CHECKLIST = '/checklist';
+export const ANALYSIS = '/analysis';
+export const REPORT = '/report';
