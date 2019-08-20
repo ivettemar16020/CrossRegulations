2e405b66f361eacda60385c91a32d96c7ddfac32
ivettemar16020 <car16020@uvg.edu.gt>
2019-08-20 11:27:21 -0600

    table and json

--- cross-reg/src/pages/Checklist/index.js
+++ cross-reg/src/pages/Checklist/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import ReactDataGrid from 'react-data-grid';
 import { Editors } from "react-data-grid-addons";
+import nist from '../../data/nist'
 import PageGuide from "./PageGuide";
 
 const { DropDownEditor } = Editors;
@@ -18,23 +19,12 @@ const columns = [
     { key: "descripcionCat", name: "Descripcion de Categoria", editable: false }, 
     { key: "subCat", name: "Subcategoria", editable: false },
     { key: "descripcionSubCat", name: "Descripcion de Subcategoria", editable: false },
-    { key: "relacion", name: "Relacion con Acuerdo 006", editable: true },
+    { key: "relación", name: "Relacion con Acuerdo 006", editable: true },
     { key: "cumplimiento", name: "Cumplimiento",  editor: IssueTypeEditor  }, 
     { key: "comentario", name: "Comentario",  editable: true  }
   ];
   
-  const rows = [
-    { key: 0, 
-        funcion: "1. Identificar", 
-        categoria: "1. Gestion de activos", 
-        descripcionCat: "Los datos, el personal, los dispositivos, los sistemas y las instalaciones que permiten a la organización alcanzar los objetivos empresariales se identifican y se administran de forma coherente con su importancia relativa para los objetivos organizativos y la estrategia de riesgos de la organización.", 
-        subCat:"ID.AM-1",
-        descripcionSubCat: "Los dispositivos y sistemas físicos dentro de la organización están inventariados", 
-        relacion: "Ninguna", 
-        cumplimiento: "En cumplimiento"},
-    { id: 1, title: "Task 2", complete: 40 },
-    { id: 2, title: "Task 3", complete: 60 }
-  ];
+  const rows = nist;
 
 class Checklist extends Component{
     state = { rows };
@@ -53,7 +43,7 @@ class Checklist extends Component{
           <ReactDataGrid
             columns={columns}
             rowGetter={i => this.state.rows[i]}
-            rowsCount={3}
+            rowsCount={50}
             onGridRowsUpdated={this.onGridRowsUpdated}
             enableCellSelect={true}
           />
